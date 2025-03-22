dfe865b5c552668e6d08e87844a64a8dbd8019f4
D P <dp@Ds-MacBook-Air.local>
2025-03-22 23:44:10 +0700

    commit

--- example/example.py
+++ example/example.py
@@ -9,41 +9,42 @@ from googleapiclient.http import MediaFileUpload
 
 st.title("Ghi âm giọng nói")
 
-# Input Name
-name = st.text_input("Name:")
+# # Input Name
+# name = st.text_input("Name:")
 
-# Input Year of Birth
+# # Input Year of Birth
 current_year = datetime.datetime.now().year
-year_of_birth = st.number_input("Year of Birth:", min_value=1900, max_value=current_year, step=1)
+# year_of_birth = st.number_input("Year of Birth:", min_value=1900, max_value=current_year, step=1)
 
-# Input Number of Years with Parkinson's
-years_with_parkinson = st.number_input("Number of Years with Parkinson's:", min_value=0, step=1)
+# # Input Number of Years with Parkinson's
+# years_with_parkinson = st.number_input("Number of Years with Parkinson's:", min_value=0, step=1)
 
-# Display the information (optional)
-if name and year_of_birth is not None and years_with_parkinson is not None: #check if all fields are filled.
-    age = current_year - year_of_birth
-    st.write(f"Name: {name}")
-    st.write(f"Age: {age}")
-    st.write(f"Years with Parkinson's: {years_with_parkinson}")
-elif name or year_of_birth is not None or years_with_parkinson is not None:
-    st.warning("Please fill out all fields.")
+# # Display the information (optional)
+# if name and year_of_birth is not None and years_with_parkinson is not None: #check if all fields are filled.
+#     age = current_year - year_of_birth
+#     st.write(f"Name: {name}")
+#     st.write(f"Age: {age}")
+#     st.write(f"Years with Parkinson's: {years_with_parkinson}")
+# elif name or year_of_birth is not None or years_with_parkinson is not None:
+#     st.warning("Please fill out all fields.")
 
-# Example with collapsed labels:
+# # Example with collapsed labels:
 
-st.subheader("Collapsed Labels Example")
+# st.subheader("Collapsed Labels Example")
 
-name_collapsed = st.text_input("Name:", label_visibility = "collapsed", placeholder = "Enter your name")
-year_collapsed = st.number_input("Year of Birth:", min_value=1900, max_value=current_year, step=1, label_visibility = "collapsed", placeholder = "Enter your birth year")
-years_collapsed = st.number_input("Years with Parkinson's:", min_value=0, step=1, label_visibility = "collapsed", placeholder = "Enter years with Parkinson's")
+name_collapsed = st.text_input("Họ tên:", label_visibility = "collapsed", placeholder = "Họ tên")
+year_collapsed = st.number_input("Năm sinh", min_value=1930, max_value=2007, step=1, label_visibility = "collapsed", placeholder = "Năm sinh")
+years_collapsed = st.number_input("Thời gian mắc bệnh Parkinson:", min_value=0, step=1, label_visibility = "collapsed", placeholder = "Thời gian mắc bệnh Parkinson")
 
 
 if name_collapsed and year_collapsed is not None and years_collapsed is not None:
     age_collapsed = current_year - year_collapsed
-    st.write(f"Name: {name_collapsed}")
-    st.write(f"Age: {age_collapsed}")
-    st.write(f"Years with Parkinson's: {years_collapsed}")
+    st.write(f"Họ tên: {name_collapsed}")
+    st.write(f"Tuổi: {age_collapsed}")
+    st.write(f"Thời gian mắc bệnh Parkinson: {years_collapsed}")
 elif name_collapsed or year_collapsed is not None or years_collapsed is not None:
-    st.warning("Please fill out all fields.")
+    # st.warning("Please fill out all fields.")
+    pass
 
 
 audio = audiorecorder("Ghi âm", "Ngừng ghi âm", "Tạm ngưng", custom_style={"backgroundColor": "lightblue"})
