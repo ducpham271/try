8a3223de545e91774be14cc7dba74b7d1a7af51d
Duc <duc23mse23145@fsb.edu.vn>
2025-04-01 20:01:21 +0700

    commit

--- example/example.py
+++ example/example.py
@@ -89,7 +89,7 @@ col7, col8 = st.columns([1, 2])
 with col7:
     st.write("Giới tính:")
 with col8:
-    gender = st.radio('', ['Nam', 'Nữ'])
+    gender = st.radio('', ['Nam', 'Nữ'], label_visibility="collapsed")
 
 col3, col4 = st.columns([1, 2])
 with col3:
