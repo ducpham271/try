b898e7085065045a5413d0b2b9cb29aced50ddb5
Duc <duc23mse23145@fsb.edu.vn>
2025-04-14 14:53:43 +0700

    commit

--- example/example.py
+++ example/example.py
@@ -72,7 +72,7 @@ st.markdown(
 )
 
 logo = Image.open("logo_new.png")
-col1a, col2a = st.columns([2, 3])  # Điều chỉnh tỷ lệ cột tùy ý
+col1a, col2a = st.columns([2, 4])  # Điều chỉnh tỷ lệ cột tùy ý
 with col1a:
     st.image(logo, width=300)
 with col2a:
